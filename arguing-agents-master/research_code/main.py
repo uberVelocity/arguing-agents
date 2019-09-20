4fe9a7eaa253920240c99c8db98ef9c7bd6c86a0
Julian <julianhegeman@gmail.com>
2019-09-20 16:32:37 +0200

    Should be able to provide a json file now and then it works?

--- arguing-agents-master/research_code/main.py
+++ arguing-agents-master/research_code/main.py
@@ -1,9 +1,14 @@
-import research
+from research import Research
 from sys import argv
+import json
 
 if len(argv) < 2 or len(argv) > 3:
     print("Usage: python3 main.py [json file]")
     exit(-1)
 
-research = Research(argv[1])
-research.run()
+
+f = open(argv[1])
+json_str = f.read()
+settings = json.loads(json_str)
+
+research = Research(settings)
--- arguing-agents-master/research_code/research.py
+++ arguing-agents-master/research_code/research.py
@@ -1,9 +1,18 @@
-import json
+from topic import Topic
 
 class Research:
-    def __init__(self, json_file):
-        f = open(json_file)
-        json_str = f.read()
-        settings = json.loads(json_str)
+    def __init__(self, research_settings):
+        if 'topics' not in research_settings:
+            print("Research: provide topics")
+            exit(-1234234)
+
+        topics = research_settings['topics']
+
+        self.topics = []
+
+        for topic_settings in topics:
+            topic = Topic(topic_settings)
+            self.topics.append(topic)
+
 
         
